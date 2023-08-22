7cc2cc8f0d2447e3518396bcd4ff2cdc57e4600f
Bipin Kishore N <68915662+bipinkish@users.noreply.github.com>
2023-08-22 18:10:16 +0530

    Add files via upload

---
+++ MothersGift/src/main/java/com/bootapp/mothersgift/MothersGiftApplication.java
@@ -0,0 +1,13 @@
+package com.bootapp.mothersgift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MothersGiftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MothersGiftApplication.class, args);
+    }
+
+}
---
+++ MothersGift/src/test/java/com/bootapp/mothersgift/MothersGiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootapp.mothersgift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MothersGiftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

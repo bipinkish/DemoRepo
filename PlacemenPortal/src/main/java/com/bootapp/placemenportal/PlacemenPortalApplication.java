66cba05c35383088c068cfcdde85ae64af552521
Bipin Kishore N <68915662+bipinkish@users.noreply.github.com>
2023-08-23 13:21:33 +0530

    Add files via upload

---
+++ PlacemenPortal/src/main/java/com/bootapp/placemenportal/PlacemenPortalApplication.java
@@ -0,0 +1,13 @@
+package com.bootapp.placemenportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacemenPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlacemenPortalApplication.class, args);
+    }
+
+}
